ce7350ad43d68b8014c2ca76b0b2ce279f73366a
Ridvan Tahir <s114074ap.be>
2021-10-14 16:28:47 +0200

    Created Initial Database

--- OfficePlanner/Server/Data/ApplicationDbContext.cs
+++ OfficePlanner/Server/Data/ApplicationDbContext.cs
@@ -7,11 +7,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using OfficePlanner.Shared;
 
 namespace OfficePlanner.Server.Data
 {
     public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
+        public DbSet<Persons> Persons { get; set; }
+        public DbSet<Reservations> Reservations { get; set; }
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<Rooms> Rooms { get; set; }
+        public DbSet<RoomVersions> RoomVersions { get; set; }
+        public DbSet<Setting> Setting { get; set; }
+        public DbSet<Users> Users { get; set; }
+
         public ApplicationDbContext(
             DbContextOptions options,
             IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
