c1775a2a2ccf928da1cec3fb41210bd90477d321
Jorn Lauwers <jornlauwers@gmail.com>
2021-10-26 01:20:08 +0200

    Role based authentication initial commit

---
+++ OfficePlanner/Server/Controllers/RoleController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OfficePlanner.Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OfficePlanner.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize(Roles = "Administrator")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+
+        // TODO: POST method for now, probably better to have the 2 roles (user, administrator) created by the application itself if they don't yet exist
+        [HttpPost("CreateRole")]
+        public async Task<IActionResult> Create(ApplicationRole role)
+        {
+            var roleExists = await roleManager.RoleExistsAsync(role.RoleName);
+            if (!roleExists)
+            {
+                var result = await roleManager.CreateAsync(new IdentityRole(role.RoleName));
+                return Ok();
+            }
+
+            return BadRequest();            
+        }
+
+    }
+}
---
+++ OfficePlanner/Server/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OfficePlanner.Server.Models;
+using OfficePlanner.Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OfficePlanner.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly UserManager<IdentityUser> userManager;
+
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+       // [Authorize]
+        [HttpPut("GiveAdmin/{Id}")]
+        public async Task<IActionResult> AddRoleToUser(string id)
+        {
+            try
+            {
+                var user = await userManager.FindByIdAsync(id);
+                userManager.AddToRoleAsync(user, "Administrator").Wait();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("GetUsers")]
+        public async Task<List<IdentityUser>> GetUsers()
+        {
+            var users = await userManager.Users.ToListAsync();
+            return users;
+        }
+
+    }
+}
