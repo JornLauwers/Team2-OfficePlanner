dd80620c84c104eadde14e0b3cc1896d31a46b46
Ridvan Tahir <s114074ap.be>
2021-11-27 21:32:38 +0100

    Listing Avaiable space

---
+++ OfficePlanner/Client/PickerTimeRange.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OfficePlanner.Client
+{
+    public class PickerTimeRange
+    {
+        public int roomId;
+        public TimeSpan min;
+        public TimeSpan max;
+    }
+}
