b0e90d9698bd268bba748877579043698b939cb9
Mitchell De Bruyn <s114600@ap.be>
2021-12-07 14:05:36 +0100

    User and Admin Added

---
+++ OfficePlanner/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OfficePlanner.Server.Data;
+using OfficePlanner.Server.Models;
+
+[assembly: HostingStartup(typeof(OfficePlanner.Server.Areas.Identity.IdentityHostingStartup))]
+namespace OfficePlanner.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
